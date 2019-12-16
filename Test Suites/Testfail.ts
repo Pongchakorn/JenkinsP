f6d00b109e00505732924c66bdd93fa70e6ad7eb
Pongchakorn <56287267+Pongchakorn@users.noreply.github.com>
2019-12-16 15:22:01 +0700

    Test Fail2

--- Test Suites/Testfail.ts
+++ Test Suites/Testfail.ts
@@ -10,4 +10,10 @@
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <testSuiteGuid>88abc6eb-55e6-4169-b706-5cbe43c7896d</testSuiteGuid>
+   <testCaseLink>
+      <guid>35150b6c-9aae-4c19-af2f-51f01093f12b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/aa</testCaseId>
+   </testCaseLink>
 </TestSuiteEntity>
